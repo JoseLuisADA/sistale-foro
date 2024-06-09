996fee9f7e323cbf56eb07c001f2f4b328de06c0
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-10 01:55:39 +0200

    ADDED ENV VARIABLES to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /*next.config.js*/
 module.exports = {
-  poweredByHeader: false
+  poweredByHeader: false,
+  env: {
+    NEXT_PUBLIC_API_SERVER_URL: process.env.NEXT_PUBLIC_API_SERVER_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+  }
 };
