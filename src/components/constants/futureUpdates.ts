753723d428b4d83e615f864d1ded0d7412a90fed
Jose Luis Luzuriaga Alanis <119702567+JoseLuisADA@users.noreply.github.com>
2024-06-10 19:55:13 +0200

    future updates added

--- src/components/constants/futureUpdates.ts
+++ src/components/constants/futureUpdates.ts
@@ -16,5 +16,9 @@ export const futureUpdates = [
   {
     title: 'Nuevo estilo de login',
     description: 'Hay un diseño a replicar de login muy accurate que en cualquier momento estará con nosotros.',
-  }
+  },
+  {
+    title: 'Nueva página de sobre nosotros y contacto',
+    description: 'Una página donde se pueda ver información sobre nosotros y un formulario de contacto para cualquier duda o sugerencia.',
+  } 
 ]
