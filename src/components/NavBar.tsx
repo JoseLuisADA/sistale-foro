e7659efaf638a7c9dac1776b37503b425b4fb2fc
Jose Luis Luzuriaga Alanis <119702567+JoseLuisADA@users.noreply.github.com>
2024-06-10 19:55:03 +0200

    styles modifieds

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -48,7 +48,7 @@ export default function Example() {
   }
 
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
