f53c4961355a729e93bc7b975d56cac3964f3856
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-10 11:23:25 +0200

    Testing save changes in auto deploy Azure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import NavBar from './NavBar';
 const Header = () => (
   <header>
     <h1 className="text-center text-4xl">
-      Sistale Foro
+      Sistale Foroo
     </h1>
     <NavBar />
   </header>
