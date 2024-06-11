271a034de6ef3275343acaf90abfbd700ed14f09
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-11 12:19:37 +0200

    Click on Logo go to Inicio

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 'use client';
 import React from 'react';
 import NavBar from './NavBar';
+import Link from 'next/link'
 
 const Header = () => (
   <header className="bg-gray-800">
+    <Link href="/">
     <img 
     className='ml-8 absolute w-16 top-1 left-5' 
     src="https://i.ibb.co/QpJfTTR/logo-removebg-preview.png"></img>
+    </Link>
     <NavBar />
   </header>
 );
