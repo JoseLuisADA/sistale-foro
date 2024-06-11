3f152134869463906eda7384401ce296d7466a92
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-11 02:22:19 +0200

    Better footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,19 +3,19 @@ import React from 'react';
 import Link from 'next/link';
 
 const Footer = () => (
-  <footer className="bg-gray-800 text-white py-8">
+  <footer className="bg-gray-800 text-white py-6 mt-4">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="mb-4 md:mb-0">
-            <h2 className="text-2xl">Sistale Foro</h2>
-            <p className="mt-2">La versión que todos han estado esperando</p>
+          <div className="mb-4 md:mb-0 text-center md:text-left">
+            <h2 className="text-xl font-bold">Sistale Foro</h2>
+            <p className="mt-2">La versión que todos esperaban</p>
           </div>
           <div className="flex space-x-4">
-            <Link href="" className="hover:underline">Sobre Nosotros</Link>
-            <Link href="" className="hover:underline">Contacto</Link>
+            <Link href="/about" className="hover:underline">Sobre Nosotros</Link>
+            <Link href="/contact" className="hover:underline">Contacto</Link>
           </div>
         </div>
-        <div className="mt-8 text-center">
+        <div className="mt-4 text-center">
           <p>&copy; {new Date().getFullYear()} Sistale Foro. Todos los derechos reservados.</p>
         </div>
       </div>
