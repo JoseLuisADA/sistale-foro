37126986222a6a8aa21f6b5b2c2321ec2eb89e20
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-12 14:06:10 +0200

    debug in recover-password route

--- src/app/api/account/public-actions/recover-password/route.tsx
+++ src/app/api/account/public-actions/recover-password/route.tsx
@@ -9,11 +9,17 @@ export async function POST(req: NextRequest) {
   try {
     const { token, newPassword } = await req.json();
 
+    console.log('TOKEN')
+    console.log(token)
+    console.log('NEW PASSWORD')
+    console.log(newPassword)
+
     const secret = new TextEncoder().encode(process.env.JWT_SECRET || '')
+    
     const { payload } = await jwtVerify(token, secret)
+    
     const { username } = payload;
 
-    // Update the user's password in the database
     await axiosInstance.put('/change-password-recovery', { username, newPassword },{
       headers: {
         'Authorization': req.headers.get('Authorization') || '',
@@ -23,7 +29,7 @@ export async function POST(req: NextRequest) {
     });
     
     return NextResponse.json({ message: 'La contraseña se ha restablecido con éxito' }, { status: 200 });
-  } catch (error: unknown) {
+  } catch (error) {
     console.error(error);
     return NextResponse.json({ message: 'Error al restablecer la contraseña' }, { status: 500 });
   }
