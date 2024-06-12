065699b00f8fea11e56a8c25566455f8bd8bf1ef
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-12 15:31:03 +0200

    design more responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -17,7 +17,6 @@ import useLogout from '../hooks/account/member-actions/useLogout'
 import { useRouter } from 'next/navigation'
 import { publicNavigation, protectedNavigation } from './constants/navbarButtons'
 
-
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
@@ -51,12 +50,11 @@ export default function Example() {
     <Disclosure as="nav">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 sm:ml-20 lg:px-8">
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5" />
+                <DisclosureButton className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -76,7 +74,7 @@ export default function Example() {
                           item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
-                        >
+                      >
                         {item.name}
                       </a>
                     ))}
@@ -87,70 +85,72 @@ export default function Example() {
                 {!user.username && (
                   <a
                     href="/login"
-                    className="relative rounded-md px-3 py-2 text-sm font-medium bg-blue-500 text-white hover:bg-blue-700">
+                    className="rounded-md px-3 py-2 text-sm font-medium bg-blue-500 text-white hover:bg-blue-700"
+                  >
                     Iniciar sesión
                   </a>
                 )}
 
                 {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  {user.username && (
-                    <>
-                      <div>
-                        <MenuButton className="relative flex rounded-full bg-gray-800 text-sm">
-                            <img
-                            className="mr-1"
-                            src="https://nosapki.com/images/nt-images/player-online.png"
-                            alt="User photo"
-                          />
-                          <p className="text-white">{user.username}</p>
-                        </MenuButton>
-                      </div>
-                      <Transition
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95">
-                        <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          <MenuItem>
-                            {({ focus }) => (
-                              <a
-                                href="/change-password"
-                                className={classNames(
-                                  focus ? 'bg-gray-100' : '',
-                                  'block px-4 py-2 text-sm text-gray-700'
-                                )}>
-                                Cambiar contraseña
-                              </a>
-                            )}
-                          </MenuItem>
-                          <MenuItem>
-                            {({ focus }) => (
-                              <a
-                                href="/login"
-                                onClick={handleLogout}
-                                className={classNames(
-                                  focus ? 'bg-gray-100' : '',
-                                  'block px-4 py-2 text-sm text-red-700'
-                                )}>
-                                Cerrar sesión
-                              </a>
-                            )}
-                          </MenuItem>
-                        </MenuItems>
-                      </Transition>
-                    </>
-                  )}
-                </Menu>
+                {user.username && (
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <MenuButton className="flex items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src="https://nosapki.com/images/nt-images/player-online.png"
+                          alt="User photo"
+                        />
+                        <p className="ml-2 text-white">{user.username}</p>
+                      </MenuButton>
+                    </div>
+                    <Transition
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <MenuItem>
+                          {({ active }) => (
+                            <a
+                              href="/change-password"
+                              className={classNames(
+                                active ? 'bg-gray-100' : '',
+                                'block px-4 py-2 text-sm text-gray-700'
+                              )}
+                            >
+                              Cambiar contraseña
+                            </a>
+                          )}
+                        </MenuItem>
+                        <MenuItem>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              onClick={handleLogout}
+                              className={classNames(
+                                active ? 'bg-gray-100' : '',
+                                'block px-4 py-2 text-sm text-red-700'
+                              )}
+                            >
+                              Cerrar sesión
+                            </a>
+                          )}
+                        </MenuItem>
+                      </MenuItems>
+                    </Transition>
+                  </Menu>
+                )}
               </div>
             </div>
           </div>
 
           <DisclosurePanel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map(item => (
+              {updatedNavigation.map(item => (
                 <DisclosureButton
                   key={item.name}
                   as="a"
@@ -159,7 +159,8 @@ export default function Example() {
                     item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                     'block rounded-md px-3 py-2 text-base font-medium'
                   )}
-                  aria-current={item.current ? 'page' : undefined}>
+                  aria-current={item.current ? 'page' : undefined}
+                >
                   {item.name}
                 </DisclosureButton>
               ))}
