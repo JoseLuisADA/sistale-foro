447443daa23fbb6eee3e1b24fae87dec5166372f
JoseLuisADA <joseluis.luzuriagaalanis.web@adaits.es>
2024-06-13 14:29:23 +0200

    Styles sections improved

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -7,7 +7,7 @@ const Sections = () => {
     <div className="container mx-auto mt-10 bg-white p-5 rounded shadow">
       <h1 className="text-4xl font-bold mb-8 text-center">Foro de Sistale</h1>
       <div className="p-5 hover:bg-yellow-100 rounded shadow-md">
-        <div className="flex">
+        <div className="flex mb-2">
           <Link href="/sections/notices-and-updates">
             <img className="object-contain absolute" src="https://i.ibb.co/YWDBc74/1218.png"></img>
             <h2 className="text-2xl font-bold mb-4 ml-12 inline">Noticias y Actualizaciones</h2>
@@ -17,7 +17,7 @@ const Sections = () => {
         <ArticulosList categoria="noticias" limit={3} />
       </div>
       <div className="p-5 bg-white rounded shadow-md mt-6 hover:bg-blue-100">
-        <div className="flex">
+        <div className="flex mb-3">
           <Link href="/sections/community">
             <img className="object-contain rounded-full absolute" src="https://nosapki.com/images/icons/5010.png"></img>
             <h2 className="text-2xl font-bold mb-4 ml-12 inline">Comunidad</h2>
